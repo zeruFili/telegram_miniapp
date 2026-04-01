732d9e5aaf170f37f25d85a7cac25b2eb9306daa
zeruFili <zerubabelfilmon1@gmail.com>
2026-04-01 18:54:38 +0300

    integrate Tailwind CSS Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
